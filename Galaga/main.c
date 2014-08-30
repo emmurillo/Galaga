62d0d9d0c506ea47a9bbbc6dffde82cf15984088
Emmurillo <emmanuel-ms@hotmail.com>
2014-08-29 19:13:47 -0600

    Modificación de la interfaz y el main

--- Galaga/main.c
+++ Galaga/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
+#include <stdio.h>
 
 static void helloWorld (GtkWidget *wid, GtkWidget *win)
 {
@@ -12,38 +13,22 @@ static void helloWorld (GtkWidget *wid, GtkWidget *win)
 }
 
 int main (int argc, char *argv[])
-{
-  GtkWidget *button = NULL;
-  GtkWidget *win = NULL;
-  GtkWidget *vbox = NULL;
+{        // User Interface
+        gtk_init (&argc, &argv);
+        GtkBuilder *builder = gtk_builder_new ();
+        gtk_builder_add_from_file (builder, "src/UI.glade", NULL);
 
-  /* Initialize GTK+ */
-  g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
-  gtk_init (&argc, &argv);
-  g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
+        GObject *window, *bot_inicio;
 
-  /* Create the main window */
-  win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_container_set_border_width (GTK_CONTAINER (win), 8);
-  gtk_window_set_title (GTK_WINDOW (win), "Hello World");
-  gtk_window_set_position (GTK_WINDOW (win), GTK_WIN_POS_CENTER);
-  gtk_widget_realize (win);
-  g_signal_connect (win, "destroy", gtk_main_quit, NULL);
+        window = gtk_builder_get_object (builder, "Vent_Principal");
+        g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
-  /* Create a vertical box with buttons */
-  vbox = gtk_vbox_new (TRUE, 6);
-  gtk_container_add (GTK_CONTAINER (win), vbox);
+        bot_inicio = gtk_builder_get_object (builder, "Bot_Inicio");
+        g_signal_connect(bot_inicio, "clicked", G_CALLBACK(helloWorld), NULL);
 
-  button = gtk_button_new_from_stock (GTK_STOCK_DIALOG_INFO);
-  g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (helloWorld), (gpointer) win);
-  gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+        gtk_widget_show_all(GTK_WIDGET(window));
 
-  button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
-  g_signal_connect (button, "clicked", gtk_main_quit, NULL);
-  gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+        gtk_main();
 
-  /* Enter the main loop */
-  gtk_widget_show_all (win);
-  gtk_main ();
-  return 0;
+        return 0;
 }
